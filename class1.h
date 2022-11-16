56d5f82cfe378c4963c183ca05fc92186ff98368
EmanuelVegaGlz <111370943+EmanuelVegaGlz@users.noreply.github.com>
2022-11-16 11:04:56 -0600

    Updates

--- class1.h
+++ class1.h
@@ -8,27 +8,37 @@ class Auto{
 private:
   string marca, modelo, color;
   float precio;
-  int year;
+  int year, potencia;
+  int transmision;
+  
 
 public:
-  Auto(): marca(""), modelo(""), year(0), color(""){};
-  Auto(string mar, int yr, string mod, string col): 
-  marca(mar), modelo(mod), year(yr), color(col){};
+  Auto(): marca(""), modelo(""), color(""), precio(0.0), 
+    year(0), potencia(0), transmision(0){};
+  
+  Auto(string mar, string mod, string col, float pre, int yr, int pot, int tra): 
+    marca(mar), modelo(mod), color(col), precio(pre),
+    year(yr), potencia(pot), transmision(tra){};
 
   string get_marca();
   string get_modelo();
-  int get_year();
   string get_color();
+  int get_year();
+  int get_potencia();
+  string get_transmision();
 
   void set_marca(string );
   void set_modelo(string );
-  void set_year(int );
   void set_color(string );
+  void set_year(int );
+  void set_potencia(int );
+  void set_transmision(int );
 
   float calcula_precio();
 
 };
 
+//Getters
 
 string Auto::get_marca(){
   return marca;
@@ -38,13 +48,26 @@ string Auto::get_modelo(){
   return modelo;
 }
 
+string Auto::get_color(){
+  return color;
+}
+
 int Auto::get_year(){
     return year;
 }
 
-string Auto::get_color(){
-  return color;
+int Auto::get_potencia(){
+    return potencia;
 }
+
+string Auto::get_transmision(){
+    if(transmision == 0)
+      return "Automatico";
+    else
+      return "Manual";
+}
+
+//Setters
 
 void Auto::set_marca(string mar){
   marca = mar;
@@ -54,13 +77,24 @@ void Auto::set_modelo(string mod){
     modelo = mod;
 }
 
+void Auto::set_color(string col){
+    color = col;
+}
+
 void Auto::set_year(int yr){
     year = yr;
 }
 
-void Auto::set_color(string col){
-    color = col;
+void Auto::set_potencia(int pot){
+    potencia = pot;
 }
+
+void Auto::set_transmision(int tran){
+    transmision = tran;
+}
+
+
+//Calcular el precio
 
 float Auto::calcula_precio(){
     if (year == 2022 && marca == "BMW")
--- main.cpp
+++ main.cpp
@@ -10,20 +10,51 @@ using namespace std;
 
 int main(){
     Auto coche1;
-    coche1.set_marca("BMW");
-    coche1.set_modelo("M4");
-    coche1.set_year(2022);
-    coche1.set_color("Azul");
-    string marca = coche1.get_marca();
-    int year = coche1.get_year();
+
+    string marc;
+    cout << "Que marca le gustaria que fuera su auto: ";
+    cin >> marc;
+    coche1.set_marca(marc);
+
+    string model;
+    cout << endl << "Escriba el modelo: ";
+    cin >> model;
+    coche1.set_modelo(model);
+
+    string col;
+    cout << endl << "En que color lo desea: ";
+    cin >> col;
+    coche1.set_color(col);
+
+    int yr;
+    cout << endl << "Digite el anio: ";
+    cin >> yr;
+    coche1.set_year(yr);
+
+    int hp;
+    cout << endl << "Cuantos caballos de fuerza quiere: ";
+    cin >> hp;
+    coche1.set_potencia(hp);
+
+    int trans;
+    cout << endl << "Hablando de la transmision" << endl
+         << " * Para automatico escriba 0" << endl 
+         << " * Para manual cualquier otro numero entero" << endl
+         << "Escriba la opcion deseada: ";
+    cin >> trans;
+    coche1.set_transmision(trans);
+   
     float price = coche1.calcula_precio();
 
-    cout << "Su pedido: " << endl;
-    cout << coche1.get_marca() << endl;
-    cout << coche1.get_modelo() << endl;
-    cout << coche1.get_year() << endl;
-    cout << coche1.get_color() << endl;
-    cout << price << endl;
+    cout << endl << "Su pedido: " << endl
+         << "Marca: " << coche1.get_marca() << endl
+         << "Modelo: " << coche1.get_modelo() << endl
+         << "Color: " << coche1.get_color() << endl
+         << "Anio: " << coche1.get_year() << endl
+         << "Caballos de fuerza: " << coche1.get_potencia() << endl
+         << "Transmision: " << coche1.get_transmision() << endl
+         << "Precio: " << price << endl
+         << endl;
 
     Agencia dealer1 ("Santiago de Queretaro", "Queretacars");
     Agencia dealer2;
@@ -37,22 +68,25 @@ int main(){
     cout << dealer2.get_nombre() << "," << dealer2.get_ubicacion() << endl;
     cout << "En que agencia desea recogerlo, escribe 1 o 2:";
     cin >> opcion;
+    cout << endl;
 
     string agencia_selec;
 
     switch (opcion)
     {
     case 1:
-        agencia_selec = dealer1.get_nombre() + dealer1.get_ubicacion();
+        agencia_selec = dealer1.get_nombre() + "," + dealer1.get_ubicacion();
         cout << "Agencia seleccionada con exito" << endl;
         break;
     case 2:
-        agencia_selec = dealer2.get_nombre() + dealer2.get_ubicacion();
+        agencia_selec = dealer2.get_nombre() + "," + dealer2.get_ubicacion();
         cout << "Agencia seleccionada con exito" << endl;
         break;
     default:
         cout << "Sucursal no encontrada";
     }
+
+    cout << endl;
 
     Asesor empleado1;
 
@@ -60,24 +94,26 @@ int main(){
     empleado1.set_celular("55 8899 0001");
     empleado1.set_correo("julian@importacionescpp.com");
 
-    cout << "Su asesor designado es: " << empleado1.get_nombre() << endl;
-    cout << "Puede contactarlo mediante los siguiente medios:" << endl;
-    cout << "Celular: " << empleado1.get_celular() << endl;
-    cout << "E-mail: " << empleado1.get_correo() << endl;
+    cout << "Su asesor designado es: " << empleado1.get_nombre() << endl
+         << "Puede contactarlo mediante los siguiente medios:" << endl
+         << "Celular: " << empleado1.get_celular() << endl
+         << "E-mail: " << empleado1.get_correo() << endl
+         << "Se encuentre en la sucursal: " << agencia_selec << endl << endl;
 
     string mail_cliente;
     cout << "Por favor dejenos su email para ponernos en contacto: ";
     cin >> mail_cliente;
-    cout << "Le enviaermos la informacion pertinente para darle continuidad" << endl;
+    cout << endl << "Le enviaermos la informacion a este correo " 
+         << mail_cliente << " para darle continuidad" << endl << endl;
 
-    cout << "Gracias por la confianza y dejarnos importar tus anhelos" <<endl;
+    cout << "Gracias por la confianza y dejarnos importar tus autos" <<endl;
 
 
-cout << "     ______        " << endl;
-cout << "    /|_||_ `.__    " << endl;
-cout << "   (   _    _ _|   " << endl;
-cout << "   =`-(_)--(_)-'   " << endl;
-    
+    cout << "     ______        " << endl
+         << "    /|_||_ `.__    " << endl
+         << "   (   _    _ _|   " << endl
+         << "   =`-(_)--(_)-'   " << endl;
+        
     return 0;
 }
 
